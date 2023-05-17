c8a9a7e21935a4852967ee087faa050296dfd8ab
Azizxon <muzaffarovazizkhon@gmail.com>
2023-05-17 14:41:08 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-center text-4xl">Salom</h1>
+</template>
+
+<style scoped>
+
+</style>
